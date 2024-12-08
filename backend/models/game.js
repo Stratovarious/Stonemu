22dc5c1d4f23bef4f072d6417041f01db33485ca
Stratovarious <mim40ist@gmail.com>
2024-12-08 13:09:45 +0300

    Create game.js

---
+++ backend/models/game.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const Game = sequelize.define('Game', {
+    game_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    game_type: {
+      type: DataTypes.STRING,
+    },
+    points_earned: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    claimed: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  }, {
+    tableName: 'games',
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  });
+
+  return Game;
+};
