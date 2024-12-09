4dd77644bef8696e187db70e5a1f3c7ada2eb03b
Stratovarious <mim40ist@gmail.com>
2024-12-09 16:55:39 +0300

    Update game.js

--- backend/models/game.js
+++ backend/models/game.js
@@ -1,29 +1,44 @@
 // models/game.js
 
-module.exports = (sequelize, DataTypes) => { 
-  const Game = sequelize.define('Game', {
-    game_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    game_type: {
-      type: DataTypes.STRING,
-    },
-    points_earned: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-    },
-    claimed: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-  }, {
-    tableName: 'games',
-    timestamps: true,
-    createdAt: 'created_at',
-    updatedAt: 'updated_at',
-  });
+module.exports = (sequelize, DataTypes) => {
+    const Game = sequelize.define('Game', {
+        game_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        user_id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            references: {
+                model: 'users',
+                key: 'user_id',
+            },
+        },
+        game_type: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        points_earned: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0,
+        },
+        claimed: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+        },
+        updated_at: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+        },
+    }, {
+        tableName: 'games',
+        timestamps: false,
+    });
 
-  return Game;
+    return Game;
 };
