716aaa65195be49fb036e9cbbb3f9ee3f1b9936a
Stratovarious <mim40ist@gmail.com>
2024-12-09 18:56:50 +0300

    Update cheatRoutes.js

--- backend/routes/cheatRoutes.js
+++ backend/routes/cheatRoutes.js
@@ -1,1 +1,11 @@
+// routes/cheatRoutes.js
 
+const express = require('express');
+const router = express.Router();
+const cheatController = require('../controllers/cheatController');
+const checkIfBanned = require('../middlewares/banChecker');
+
+// Örnek: Hile Raporlama
+router.post('/:user_id/report', cheatController.reportCheat);
+
+module.exports = router;
