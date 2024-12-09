2dd487c14f3500af5e49946a14d20d408e80abd1
Stratovarious <mim40ist@gmail.com>
2024-12-09 16:50:39 +0300

    Update index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -4,6 +4,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
+  logging: console.log, // SQL loglarını etkinleştir
   dialectOptions: {
     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
   },
