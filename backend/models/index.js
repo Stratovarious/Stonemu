cb37c91264b3240e33fc7e80816915c0a02fc95d
Stratovarious <mim40ist@gmail.com>
2024-12-09 19:07:46 +0300

    Update index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -22,13 +22,13 @@ db.Cheat = require('./cheat')(sequelize, Sequelize);
 db.Warning = require('./warning')(sequelize, Sequelize);
 
 // Associations
-db.User.hasMany(db.Game, { foreignKey: 'user_id' });
+db.User.hasMany(db.Game, { foreignKey: 'user_id', onDelete: 'CASCADE' });
 db.Game.belongsTo(db.User, { foreignKey: 'user_id' });
 
-db.User.hasMany(db.Cheat, { foreignKey: 'user_id' });
+db.User.hasMany(db.Cheat, { foreignKey: 'user_id', onDelete: 'CASCADE' });
 db.Cheat.belongsTo(db.User, { foreignKey: 'user_id' });
 
-db.User.hasMany(db.Warning, { foreignKey: 'user_id' });
+db.User.hasMany(db.Warning, { foreignKey: 'user_id', onDelete: 'CASCADE' });
 db.Warning.belongsTo(db.User, { foreignKey: 'user_id' });
 
 module.exports = db;
