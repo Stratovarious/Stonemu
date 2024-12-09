71da90cc4e65b4b594ef7bfc21eea85cc36f8c40
Stratovarious <mim40ist@gmail.com>
2024-12-09 19:15:18 +0300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,14 @@
+// config/db.js
 
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: console.log, // SQL loglarını etkinleştir
+  dialectOptions: {
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  },
+});
+
+module.exports = sequelize;
