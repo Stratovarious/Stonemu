7335c05622bdb6744e57a6ff98044e88621e622b
Stratovarious <mim40ist@gmail.com>
2024-12-09 19:17:09 +0300

    Update index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,21 +1,12 @@
 // models/index.js
 
-const { Sequelize } = require('sequelize'); 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  logging: console.log, // SQL loglarını etkinleştir
-  dialectOptions: {
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
-  },
-});
-
+const sequelize = require('../config/db');
+const { Sequelize } = require('sequelize');
 const db = {};
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-// Corrected require paths
 db.User = require('./users')(sequelize, Sequelize);
 db.Game = require('./game')(sequelize, Sequelize);
 db.Cheat = require('./cheat')(sequelize, Sequelize);
