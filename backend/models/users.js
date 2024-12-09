26a39e885fd998d47d8a2e26f23ca25b3701b946
Stratovarious <mim40ist@gmail.com>
2024-12-09 16:54:40 +0300

    Update users.js

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,52 +1,60 @@
 // models/users.js
 
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    user_id: {
-      type: DataTypes.STRING,
-      primaryKey: true,
-    },
-    username: {
-      type: DataTypes.STRING,
-    },
-    points: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-    },
-    a: { // Click rights
-      type: DataTypes.INTEGER,
-      defaultValue: 5000,
-    },
-    b: { // Maximum click rights
-      type: DataTypes.INTEGER,
-      defaultValue: 5000,
-    },
-    dolum_hizi: { // Refill speed (in seconds)
-      type: DataTypes.INTEGER,
-      defaultValue: 10,
-    },
-    tiklama_hakki: { // Points added per click
-      type: DataTypes.INTEGER,
-      defaultValue: 1,
-    },
-    cheat_count: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-    },
-    is_banned: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-    last_a_update: { // Added as per SQL schema
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-    },
-  }, {
-    tableName: 'users',
-    timestamps: true,
-    createdAt: 'created_at',
-    updatedAt: 'updated_at',
-  });
+    const User = sequelize.define('User', {
+        user_id: {
+            type: DataTypes.STRING,
+            primaryKey: true,
+            allowNull: false,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        points: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0,
+        },
+        cheat_count: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0,
+        },
+        is_banned: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        },
+        last_a_update: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+        },
+        a: {
+            type: DataTypes.INTEGER,
+            defaultValue: 5000,
+        },
+        b: {
+            type: DataTypes.INTEGER,
+            defaultValue: 5000,
+        },
+        dolum_hizi: {
+            type: DataTypes.INTEGER,
+            defaultValue: 10,
+        },
+        tiklama_hakki: {
+            type: DataTypes.INTEGER,
+            defaultValue: 1,
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+        },
+        updated_at: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+        },
+    }, {
+        tableName: 'users',
+        timestamps: false, // initialize.sql'de created_at ve updated_at var
+    });
 
-  return User;
+    return User;
 };
