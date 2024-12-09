0a4a6f8f032e9de69270448e437238e539ab4cb8
Stratovarious <mim40ist@gmail.com>
2024-12-09 18:55:57 +0300

    Update gameRoutes.js

--- backend/routes/gameRoutes.js
+++ backend/routes/gameRoutes.js
@@ -1,1 +1,14 @@
+// routes/gameRoutes.js
 
+const express = require('express');
+const router = express.Router();
+const gameController = require('../controllers/gameController');
+const checkIfBanned = require('../middlewares/banChecker');
+
+// Örnek: Oyun Başlatma
+router.post('/start', gameController.startGame);
+
+// Örnek: Oyun Sonlandırma
+router.post('/end', gameController.endGame);
+
+module.exports = router;
