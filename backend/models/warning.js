6c83be7178e711678043b1f9d54b20a897b8b555
Stratovarious <mim40ist@gmail.com>
2024-12-09 15:45:03 +0300

    Update warning.js

--- backend/models/warning.js
+++ backend/models/warning.js
@@ -1,9 +1,20 @@
+// models/warning.js
+
 module.exports = (sequelize, DataTypes) => {
   const Warning = sequelize.define('Warning', {
     warning_id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
+    },
+    user_id: { // Added to align with SQL schema
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'user_id',
+      },
+      onDelete: 'CASCADE',
     },
     message: {
       type: DataTypes.TEXT,
