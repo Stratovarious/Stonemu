570180bbc05d6021c4c44338cb918af493bad3f9
Stratovarious <mim40ist@gmail.com>
2024-12-09 18:52:27 +0300

    Update rateLimiter.js

--- backend/middlewares/rateLimiter.js
+++ backend/middlewares/rateLimiter.js
@@ -1,1 +1,11 @@
+// middlewares/rateLimiter.js
 
+const rateLimit = require('express-rate-limit');
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 dakika
+  max: 100, // Her IP için maksimum 100 istek
+  message: 'Çok fazla istek gönderildi, lütfen daha sonra tekrar deneyin.',
+});
+
+module.exports = apiLimiter;
