1e9a3ae7465993619dd709eb993f3d50975457ab
Stratovarious <mim40ist@gmail.com>
2024-12-09 15:44:02 +0300

    Update cheat.js

--- backend/models/cheat.js
+++ backend/models/cheat.js
@@ -1,9 +1,20 @@
+// models/cheat.js
+
 module.exports = (sequelize, DataTypes) => {
   const Cheat = sequelize.define('Cheat', {
     cheat_id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
+    },
+    user_id: { // Added to align with SQL schema
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'user_id',
+      },
+      onDelete: 'CASCADE',
     },
     cheat_type: {
       type: DataTypes.STRING,
