ebb64e48e78e969b0b55d41c25679ef44679c0d5
Stratovarious <mim40ist@gmail.com>
2024-12-09 19:01:04 +0300

    Update userService.js

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,1 +1,12 @@
+// services/userService.js
 
+const db = require('../models');
+
+// WebSocket üzerinden kullanıcıya bildirim gönderme
+exports.notifyUserOfCheat = (user_id, message) => {
+  // Bu fonksiyon, Socket.io üzerinden kullanıcıya bildirim göndermek için kullanılacak.
+  // Bunun için Socket.io bağlantılarının burada erişilebilir olması gerekiyor.
+  // Bu implementasyon, gameSocket modülünde yapılacak.
+  // Bu nedenle, bu fonksiyonun Socket.io entegrasyonuna göre güncellenmesi gerekmektedir.
+  // Alternatif olarak, Socket.io bağlantılarını burada yönetebilirsiniz.
+};
