27e2251bcdc64f409209b4372ddf38e10ca5cc2e
Stratovarious <mim40ist@gmail.com>
2024-12-09 18:53:26 +0300

    Update banChecker.js

--- backend/middlewares/banChecker.js
+++ backend/middlewares/banChecker.js
@@ -1,1 +1,21 @@
+// middlewares/banChecker.js
 
+const db = require('../models');
+
+const checkIfBanned = async (req, res, next) => {
+  try {
+    const { user_id } = req.params;
+    const userIdStr = String(user_id); // user_id'yi string'e dönüştür
+
+    const user = await db.User.findByPk(userIdStr);
+    if (user && user.is_banned) {
+      return res.status(403).json({ error: 'Hesabınız banlanmıştır.' });
+    }
+    next();
+  } catch (error) {
+    console.error("Ban kontrolü sırasında hata:", error);
+    res.status(500).json({ error: 'Sunucu hatası.' });
+  }
+};
+
+module.exports = checkIfBanned;
